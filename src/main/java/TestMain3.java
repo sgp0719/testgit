47588023de40910c22c278ae1b1504db5f8898d1
Administrator <Administrator@10.6.205.245>
2020-06-04 11:28:20 +0800

    four commit

---
+++ src/main/java/TestMain3.java
@@ -0,0 +1,12 @@
+package main.java;
+
+public class TestMain3 {
+    private static TestMain3 ourInstance = new TestMain3();
+
+    public static TestMain3 getInstance() {
+        return ourInstance;
+    }
+
+    private TestMain3() {
+    }
+}
